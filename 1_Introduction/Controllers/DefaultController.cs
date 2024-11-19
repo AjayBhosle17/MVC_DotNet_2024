f9f9fd8fb01c8057dbdc73e29977ddbcc072f2d9
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-11-19 14:16:10 +0530

    MVC INTRO

---
+++ 1_Introduction/Controllers/DefaultController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1_Introduction.Controllers
+{
+    [RoutePrefix("Default")]
+
+    public class DefaultController : Controller
+    {
+        // GET: Default
+
+        [Route("Ajju")]
+        public ActionResult Index1()
+        {
+            return View();
+        }
+
+        [Route("Details/Ajju")]
+        public ActionResult Group() {
+
+            return View();
+        }
+    }
+}
---
+++ 1_Introduction/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1_Introduction.Controllers
+{
+    /*[RoutePrefix("home")]*/
+    public class ProductController : Controller
+    {
+        // GET: Product
+
+        [Route("")]
+        public string Index()
+        {
+            return "hii";
+        }
+    }
+}
