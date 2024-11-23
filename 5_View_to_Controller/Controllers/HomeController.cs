6d6670daf1e4d29e1e9c65c741bd74519f54656d
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-11-24 04:12:19 +0530

    View to COntroller

---
+++ 5_View_to_Controller/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5_View_to_Controller.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details(int productId, string Name , int Price)
+        {
+            ViewBag.productId = productId;
+            return View();
+        }
+    }  
+}
---
+++ 5_View_to_Controller/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_5_View_to_Controller.MvcApplication" Language="C#" %>
