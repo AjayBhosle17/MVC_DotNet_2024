dd4a6502cee039cb9e52c118ae2c39af5cda02b1
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-12-08 03:30:20 +0530

    Using EF All Crud Operation Code first Approch

--- 10_Code_First_Approch/Controllers/CategoryController.cs
+++ 10_Code_First_Approch/Controllers/CategoryController.cs
@@ -23,5 +23,100 @@ namespace _10_Code_First_Approch.Controllers
             List<Category> categories = _dbContext.categories.ToList();
             return View(categories);
         }
+
+        [HttpGet]
+
+        public ActionResult Create() {
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Create(Category category) {
+
+            _dbContext.categories.Add(category);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+
+        public ActionResult Details(int id) {
+
+            Category category = _dbContext.categories.Find(id);
+
+            return View(category);
+
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id) {
+
+            Category category = _dbContext.categories.Find(id);
+
+            return View(category);
+
+
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+
+            Category category = _dbContext.categories.Find(id);
+            if (id == null)
+            {
+
+                return View(category);
+            }
+            else
+            {
+
+                _dbContext.categories.Remove(category);
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        [HttpGet]
+
+        public ActionResult Edit(int? id) {
+
+            Category category = _dbContext.categories.Find(id);
+            return View(category);
+        }
+
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+
+            Category category1 = _dbContext.categories.Find(category.Id);
+
+            if (category == null)
+            {
+
+                return View(category1);
+            }
+            else
+            {
+
+                category1.Name = category.Name;
+                category1.Rating = category.Rating;
+
+
+                _dbContext.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+
+        }
     }
+
+
 }
