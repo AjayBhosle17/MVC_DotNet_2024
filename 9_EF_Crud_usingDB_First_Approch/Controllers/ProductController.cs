b3e03a4318d4cc4942d779bd831a2492bd6b6220
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-12-07 12:50:20 +0530

    Using EF All Crud Operation DB first Approch

--- 9_EF_Crud_usingDB_First_Approch/Controllers/ProductController.cs
+++ 9_EF_Crud_usingDB_First_Approch/Controllers/ProductController.cs
@@ -63,5 +63,91 @@ namespace _9_EF_Crud_usingDB_First_Approch.Controllers
             }
             return View(product);
         }
+
+        [HttpGet]
+
+        public ActionResult Delete(int id) {
+
+            Product product = _db.Products.Find(id);
+
+            // 2 ways 
+            /* 1. without post
+
+                 if (product != null)
+             {
+                 _db.Products.Remove(product);
+                 _db.SaveChanges();
+                 return RedirectToAction("Index");
+             }*/
+
+            //  2. with post
+
+            if (product == null) {
+
+                ViewBag.name = "No Product Availble";
+            }
+            return View(product);
+
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            try
+            {
+                Product product = _db.Products.Find(id);
+
+                if (product == null)
+                {
+                    ViewBag.name = "No Product Availble";
+                }
+
+                _db.Products.Remove(product);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch (Exception) { 
+            
+                return RedirectToAction("Index");
+            }
+
+        }
+
+        [HttpGet]
+
+        public ActionResult Edit(int id) {
+
+            Product product = _db.Products.Find(id);
+
+            if (product == null)
+            {
+
+                ViewBag.name = "No Product Availble";
+            }
+            return View(product);
+
+        }
+
+        [HttpPost]
+
+        public ActionResult Edit(Product product) { 
+        
+            Product p = _db.Products.Find(product.Id);
+            if (p != null) { 
+            
+                p.Name = product.Name;
+                p.Price = product.Price;
+
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return ViewBag.name = "No Product Availble";
+            }
+
+        }
+        
     }
 }
