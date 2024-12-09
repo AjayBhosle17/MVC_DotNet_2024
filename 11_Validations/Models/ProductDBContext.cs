7e55a84822116440fd2462a2d8109ee14e1b8122
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-12-09 19:31:22 +0530

    Validation in MVC

---
+++ 11_Validations/Models/ProductDBContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace _11_Validations.Models
+{
+    public class UserDBContext:DbContext
+    {
+        public UserDBContext(): base("name=ValidationDB") {
+        
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
