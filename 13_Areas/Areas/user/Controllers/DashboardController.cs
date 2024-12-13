e126eae6130814163741bf80c75cd42d093e77d6
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-12-13 10:10:30 +0530

    Area Login Model

---
+++ 13_Areas/Areas/user/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _13_Areas.Areas.user.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: user/Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- 13_Areas/Controllers/AccountController.cs
+++ 13_Areas/Controllers/AccountController.cs
@@ -1,17 +1,53 @@
-﻿using System;
+﻿using _13_Areas.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 using System.Web.Mvc;
+using _13_Areas.Models;
 
 namespace _13_Areas.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
+        UserDbContext _userDBContext =new UserDbContext();
+
+        [HttpGet]
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Login(LoginModel lgmodel)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isAuthenticated = _userDBContext.LoginData.Any(u=>u.Email.Equals(lgmodel.Email)&& u.Password.Equals(lgmodel.Password));
+
+                if (isAuthenticated) {
+
+                    if (lgmodel.Email.Contains("bhosle"))
+                    {
+                        return RedirectToAction("Index", "Dashboard", new { area = "admin" });
+
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Dashboard", new { area = "user" });
+
+                    }
+                }
+            }
+           
+            
+                ViewBag.Message = "Login Failed";
+                return View();
+        
+
         }
     }
 }
