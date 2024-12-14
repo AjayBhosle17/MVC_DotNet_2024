830f7887be15ba1edc589680d61c1228c3096974
Ajaybhosle17 <ajaybhosle.comp@gmail.com>
2024-12-14 11:30:55 +0530

    Filter In Mvc5

---
+++ 14_Filters/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _14_Filters.Controllers
+{
+   /* [Authorize]*/
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [AllowAnonymous]
+        public ActionResult Index(string comment)
+        {
+            ViewBag.Comment = comment;  
+            return View();
+        }
+
+        public ActionResult Index1()
+        {
+            return View();
+        }
+       // [RequireHttps]
+        [AllowAnonymous]
+        public ActionResult Index2()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult AddPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddPost(string comment)
+        {
+            return RedirectToAction("Index", new {comment=comment});
+        }
+
+        [HttpGet]
+        [ChildActionOnly]
+        public PartialViewResult getPartialViewContent()
+        {
+            return PartialView("_PartialView");
+        }
+    }
+}
